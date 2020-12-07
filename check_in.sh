8ca1540f64bc1ac25fb7800c92f75b87fc8a1694
Pavel Logacev <pavel.logacev@gmail.com>
2020-12-07 22:55:42 +0300

    update

--- check_in.sh
+++ check_in.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#!/bin/bash
+rm -r docs/lecture_notes/R/*
+rm -r docs/lecture_notes/stats/*
 cp -r docs/lecture_notes/R_source/* docs/lecture_notes/R/
 cp -r docs/lecture_notes/stats_source/* docs/lecture_notes/stats/
 git add *
